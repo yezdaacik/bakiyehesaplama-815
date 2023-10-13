249931c4b2a0dc788046415dadf561d4fdfb7916
yezdanuracik <145451081+yezdanuracik@users.noreply.github.com>
2023-10-13 18:31:17 +0300

    Add files via upload

---
+++ bakiye hesaplama/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace bakiye_hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblBakiye = new System.Windows.Forms.Label();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.rb3 = new System.Windows.Forms.RadioButton();
+            this.rb4 = new System.Windows.Forms.RadioButton();
+            this.btnOde = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bakiye";
+            // 
+            // lblBakiye
+            // 
+            this.lblBakiye.AutoSize = true;
+            this.lblBakiye.Location = new System.Drawing.Point(61, 31);
+            this.lblBakiye.Name = "lblBakiye";
+            this.lblBakiye.Size = new System.Drawing.Size(73, 13);
+            this.lblBakiye.TabIndex = 1;
+            this.lblBakiye.Text = "___________";
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Location = new System.Drawing.Point(19, 72);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(58, 17);
+            this.rb1.TabIndex = 2;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "1. biniş";
+            this.rb1.UseVisualStyleBackColor = true;
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(19, 95);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(58, 17);
+            this.rb2.TabIndex = 3;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "2. biniş";
+            this.rb2.UseVisualStyleBackColor = true;
+            // 
+            // rb3
+            // 
+            this.rb3.AutoSize = true;
+            this.rb3.Location = new System.Drawing.Point(19, 118);
+            this.rb3.Name = "rb3";
+            this.rb3.Size = new System.Drawing.Size(58, 17);
+            this.rb3.TabIndex = 4;
+            this.rb3.TabStop = true;
+            this.rb3.Text = "3. biniş";
+            this.rb3.UseVisualStyleBackColor = true;
+            // 
+            // rb4
+            // 
+            this.rb4.AutoSize = true;
+            this.rb4.Location = new System.Drawing.Point(19, 141);
+            this.rb4.Name = "rb4";
+            this.rb4.Size = new System.Drawing.Size(58, 17);
+            this.rb4.TabIndex = 5;
+            this.rb4.TabStop = true;
+            this.rb4.Text = "4. biniş";
+            this.rb4.UseVisualStyleBackColor = true;
+            // 
+            // btnOde
+            // 
+            this.btnOde.Location = new System.Drawing.Point(12, 177);
+            this.btnOde.Name = "btnOde";
+            this.btnOde.Size = new System.Drawing.Size(92, 23);
+            this.btnOde.TabIndex = 6;
+            this.btnOde.Text = "Öde";
+            this.btnOde.UseVisualStyleBackColor = true;
+            this.btnOde.Click += new System.EventHandler(this.btnOde_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(136, 215);
+            this.Controls.Add(this.btnOde);
+            this.Controls.Add(this.rb4);
+            this.Controls.Add(this.rb3);
+            this.Controls.Add(this.rb2);
+            this.Controls.Add(this.rb1);
+            this.Controls.Add(this.lblBakiye);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblBakiye;
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.RadioButton rb3;
+        private System.Windows.Forms.RadioButton rb4;
+        private System.Windows.Forms.Button btnOde;
+    }
+}
+
---
+++ bakiye hesaplama/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bakiye_hesaplama
+{
+    public partial class Form1 : Form
+    {
+        double bakiye = 200;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOde_Click(object sender, EventArgs e)
+        {
+          if (bakiye >= 15)
+            {
+                bakiye -= 15;
+                lblBakiye.Text = Convert.ToString(bakiye);
+            }
+
+          else if (bakiye >= 12.20)
+            {
+                bakiye -= 12.20;
+                lblBakiye.Text = Convert.ToString(bakiye);
+            }
+
+          else if (bakiye >= 9.60)
+            {
+                bakiye -= 9.60;
+                lblBakiye.Text = Convert.ToString(bakiye);
+            }
+
+          else if (bakiye >= 8)
+            {
+                bakiye-= 8;
+                lblBakiye.Text= Convert.ToString(bakiye);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblBakiye.Text = Convert.ToString(bakiye);
+        }
+    }
+}
